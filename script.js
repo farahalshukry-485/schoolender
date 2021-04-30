29be4314e4d9b92d8f154bc47f4ef3ca446deea8
farahalshukry-485 <66687774+farahalshukry-485@users.noreply.github.com>
2021-05-01 02:29:09 +0300

    done

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ var password = document.getElementById("password")
   , confirm_password = document.getElementById("confirm_password");
 
 function validatePassword(){
-  if(password.value != confirm_password.value) {
+  if(password.value != confirm_password.value) { 
     confirm_password.setCustomValidity("Passwords Don't Match");
   } else {
     confirm_password.setCustomValidity('');
-  }
-}
+   
+        window.location.href="newpage.html";
+    } 
+   }
+
 
 password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
